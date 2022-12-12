f88500c579c05ed3384c65186be33baf050c4d43
Filomena <filomenapteles96@gmail.com>
2022-12-12 11:36:26 +0000

    criaçao da entidade livro

---
+++ GestaoLivrosApi/GestaoLivrosApi/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GestaoLivrosApi.Data
+{
+	public class AppDbContext
+	{
+		public AppDbContext()
+		{
+		}
+	}
+}
+
---
+++ GestaoLivrosApi/GestaoLivrosApi/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestaoLivrosApi.Models
+{
+	public class Book
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public int Isbn { get; set; }
+
+		[Required]
+		public string Name { get; set; } = null!;
+
+        [Required]
+        public string Author { get; set; } = null!;
+
+        [Required]
+		public double Price { get; set; }
+    }
+}
+
