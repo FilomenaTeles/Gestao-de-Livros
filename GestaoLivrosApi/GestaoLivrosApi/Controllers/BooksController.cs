7047afb0b26cfe19a994cf7f27d8be19921c1fa8
Filomena <filomenapteles96@gmail.com>
2022-12-12 12:58:12 +0000

    criação da Api e implementação do metodo Get

---
+++ GestaoLivrosApi/GestaoLivrosApi/Controllers/BooksController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GestaoLivrosApi.Services;
+using GestaoLivrosApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GestaoLivrosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+        private IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+
+        // GET: api/values
+        [HttpGet]
+       public async Task<ActionResult<IAsyncEnumerable<Book>>> GetBooks()
+        {
+            try
+            {
+                var books = await _bookService.GetBooks();
+                return Ok(books);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter alunos");
+
+            }
+        }
+
+    }
+}
+
---
+++ GestaoLivrosApi/GestaoLivrosApi/Services/IBookService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using GestaoLivrosApi.Models;
+namespace GestaoLivrosApi.Services
+{
+	public interface IBookService
+	{
+		//Get livros
+		Task<IEnumerable<Book>> GetBooks();
+	}
+}
+
