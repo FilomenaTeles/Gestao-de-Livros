d7fbe0c84c6db0ff773c677d856a045ee05038ee
Filomena <filomenapteles96@gmail.com>
2022-12-15 15:39:17 +0000

    criação da chamada api para inserir novo livro

--- frontend/src/components/Books/AddBook.tsx
+++ frontend/src/components/Books/AddBook.tsx
@@ -2,21 +2,53 @@ import api from "../../services/api";
 import "./styles.css"
 
 import {BsBook} from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, redirect } from "react-router-dom";
+import { ChangeEvent, useEffect, useState } from "react";
 
 
 export function AddBook(){
+
+    const navigate = useNavigate();
+    const [newBook,setNewBook]= useState({
+        name: '',
+        author: '',
+        isbn: 0,
+        price: 0.0,
+    });
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setNewBook({
+          ...newBook,[name]:value
+        });
+        console.log(newBook);
+      }
+
+  async function postRequest(event: any) {
+    try{
+        await api.post('api/Books',newBook);
+       
+          
+    }catch(error){
+            alert('Erro ao adicionar novo livro: '+error);
+    }
+    // navigate('/books');
+    redirect('/books');
+  }
+
+ /*  useEffect(()=>{
+    
+  }); */
     return(
         <div className="add-book-container">
            <div className="container p-3 mt-5">
             <h3 className="text-center"><BsBook size={55}/> Adicionar novo Livro</h3>
             <div className="container row">
                 <div className="container col-6">
-                    <form>
-                        <input className="input-group m-3" type="text" placeholder="Nome"/>
-                        <input className="input-group m-3"  type="text" placeholder="Autor" />
-                        <input className="input-group m-3"  type="number" placeholder="ISBN"/>
-                        <input className="input-group m-3"  type="number" placeholder="Preço"/>
+                    <form onSubmit={postRequest}>
+                        <input className="input-group m-3" type="text" placeholder="Nome" name="name" onChange={handleChange} />
+                        <input className="input-group m-3"  type="text" placeholder="Autor" name="author" onChange={handleChange}/>
+                        <input className="input-group m-3"  type="number" placeholder="ISBN" name="isbn" onChange={handleChange}/>
+                        <input className="input-group m-3"  type="double" placeholder="Preço" name="price" onChange={handleChange}/>
                         {/* <input className="input-group m-3" type="file" /> */}
                         <button className="btn btn-success ms-3" type="submit" >Adicionar</button>
                         <Link className="btn btn-danger m-2" to='/books'>Cancelar</Link>
